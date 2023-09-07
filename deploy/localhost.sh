74575346d48724e86c9674432c4963756fb789f4
Avinash Kumar <2010215@sliet.ac.in>
2023-09-07 20:08:47 +0000

    fix

--- deploy/localhost.sh
+++ deploy/localhost.sh
@@ -1,7 +1,5 @@
-# cd clientSide/project
-# npm run build
-
+cd clientSide/project
+npm run build
 cd environments
 cp frontendEnvLocal.js frontendEnv.js 
-
-# firebase emulators:start
+firebase emulators:start
